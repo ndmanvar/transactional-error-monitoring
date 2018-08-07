f1b109441eb41aaa5baf9ea88d34273c2b037bc2
Neil Manvar <neil@sentry.io>
2018-08-07 16:50:06 -0700

    add raven middleware

--- react-redux-example-app/src/store.js
+++ react-redux-example-app/src/store.js
@@ -1,8 +1,13 @@
+/*global undefinedVariable:false Raven:false*/
+/*eslint no-unused-vars:0 no-eval:0*/
+
 import { applyMiddleware, createStore } from 'redux';
 import { createLogger } from 'redux-logger'
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import { promiseMiddleware, localStorageMiddleware } from './middleware';
 import reducer from './reducer';
+
+import createRavenMiddleware from "raven-for-redux";
 
 import { routerMiddleware } from 'react-router-redux'
 import createHistory from 'history/createBrowserHistory';
@@ -12,12 +17,22 @@ export const history = createHistory();
 // Build the middleware for intercepting and dispatching navigation actions
 const myRouterMiddleware = routerMiddleware(history);
 
+
+
 const getMiddleware = () => {
   if (process.env.NODE_ENV === 'production') {
-    return applyMiddleware(myRouterMiddleware, promiseMiddleware, localStorageMiddleware);
+    return applyMiddleware(myRouterMiddleware, promiseMiddleware, localStorageMiddleware, createRavenMiddleware(Raven, {
+        breadcrumbDataFromAction: action => {
+            return { STRING: action.str };
+        }
+    }));
   } else {
     // Enable additional logging in non-production environments.
-    return applyMiddleware(myRouterMiddleware, promiseMiddleware, localStorageMiddleware, createLogger())
+    return applyMiddleware(myRouterMiddleware, promiseMiddleware, localStorageMiddleware, createLogger(), createRavenMiddleware(Raven ,{
+        breadcrumbDataFromAction: action => {
+            return { STRING: action.str };
+        }
+  }));
   }
 };
 
